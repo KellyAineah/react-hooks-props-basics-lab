1933e1fd0e7f37a92649f9880253a24d06e988d2
KellyAineah <kellyainea99@gmail.com>
2024-04-24 20:57:48 +0300

    added props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {name, city} from "../data/user";
 
 function Home(props) {
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
+        
       </h1>
     </div>
   );
